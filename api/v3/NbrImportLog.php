d92d870513590a05377e74f5b1e11da6b0e3c7a7
ErikHommel <erik.hommel@civicoop.org>
2019-10-21 13:19:51 +0200

    fix issue 4950 - add import log page

---
+++ api/v3/NbrImportLog.php
@@ -0,0 +1,81 @@
+<?php
+use CRM_Nihrbackbone_ExtensionUtil as E;
+
+/**
+ * NbrImportLog.create API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
+ */
+function _civicrm_api3_nbr_import_log_create_spec(&$spec) {
+  $spec['import_id'] = [
+    'name' => 'import_d',
+    'title' => 'Import ID',
+    'description' => 'Unique Import ID',
+    'api.required' => 1,
+    'type' => CRM_Utils_Type::T_STRING,
+  ];
+  $spec['filename'] = [
+    'name' => 'filename',
+    'title' => 'Import file',
+    'description' => 'Import file',
+    'api.required' => 0,
+    'type' => CRM_Utils_Type::T_STRING,
+  ];
+  $spec['filename'] = [
+    'name' => 'filename',
+    'title' => 'Import file',
+    'description' => 'Import file',
+    'api.required' => 0,
+    'type' => CRM_Utils_Type::T_STRING,
+  ];
+  $spec['message'] = [
+    'name' => 'message',
+    'title' => 'Message',
+    'description' => 'Message',
+    'api.required' => 1,
+    'type' => CRM_Utils_Type::T_TEXT,
+  ];
+  $spec['logged_date'] = [
+    'name' => 'logged_date',
+    'title' => 'Date Logged',
+    'description' => 'Date Logged',
+    'api.required' => 1,
+    'type' => CRM_Utils_Type::T_DATE,
+  ];
+}
+
+/**
+ * NbrImportLog.create API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @throws API_Exception
+ */
+function civicrm_api3_nbr_import_log_create($params) {
+  return _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+}
+
+/**
+ * NbrImportLog.delete API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @throws API_Exception
+ */
+function civicrm_api3_nbr_import_log_delete($params) {
+  return _civicrm_api3_basic_delete(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+}
+
+/**
+ * NbrImportLog.get API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @throws API_Exception
+ */
+function civicrm_api3_nbr_import_log_get($params) {
+  return _civicrm_api3_basic_get(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+}
---
+++ xml/schema/CRM/Nihrbackbone/NbrImportLog.entityType.php
@@ -0,0 +1,11 @@
+<?php
+// This file declares a new entity type. For more details, see "hook_civicrm_entityTypes" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'NbrImportLog',
+    'class' => 'CRM_Nihrbackbone_DAO_NbrImportLog',
+    'table' => 'civicrm_nbr_import_log',
+  ),
+);
