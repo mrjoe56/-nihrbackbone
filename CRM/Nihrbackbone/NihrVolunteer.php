63205571fb64a25567caffd78328aed493774ab9
ErikHommel <erik.hommel@civicoop.org>
2019-10-22 09:24:50 +0200

    fix test condition max projects

--- CRM/Nihrbackbone/NihrVolunteer.php
+++ CRM/Nihrbackbone/NihrVolunteer.php
@@ -137,21 +137,26 @@ class CRM_Nihrbackbone_NihrVolunteer {
     // get the settings for the max number, the max period and the case status to be counted
     $maxNumber = Civi::settings()->get('nbr_max_participations');
     $noMonths = Civi::settings()->get('nbr_no_months_participation');
-    $countCaseStatuses = Civi::settings()->get('nbr_part_case_status');
+    $countCaseStatuses = (int) Civi::settings()->get('nbr_part_case_status');
+    // make case statuses array if not array
+    if (!is_array($countCaseStatuses)) {
+      $countCaseStatuses = [$countCaseStatuses];
+    }
     $checkDate = new DateTime();
     $modifier = '-' . $noMonths . ' months';
     $checkDate->modify($modifier);
     // retrieve the number of participation cases in the specified period with the status that
     // are to be counted
+    $caseParams = [
+      'case_type_id' => CRM_Nihrbackbone_BackboneConfig::singleton()->getParticipationCaseTypeId(),
+      'start_date' => ['>' => $checkDate->format('d-m-Y')],
+      'status_id' => ['IN' => $countCaseStatuses],
+      'contact_id' => $contactId,
+      'is_deleted' => 0,
+    ];
     try {
-      $result = (int)civicrm_api3('Case', 'getcount', [
-        'case_type_id' => CRM_Nihrbackbone_BackboneConfig::singleton()->getParticipationCaseTypeId(),
-        'start_date' => ['>' => $checkDate->format('d-m-Y')],
-        'status_id' => ['IN' => $countCaseStatuses],
-        'contact_id' => $contactId,
-        'is_deleted' => 0,
-      ]);
-      if ($result >= $maxNumber) {
+      $result = (int)civicrm_api3('Case', 'getcount', $caseParams);
+      if ($result > $maxNumber) {
         return TRUE;
       }
     }
