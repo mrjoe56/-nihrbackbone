7e37c217bb91a3d11e1101423802c50252e14241
jab1012 <john.boucher@bioresource.nihr.ac.uk>
2020-06-01 14:21:05 +0100

    JB new report templates

---
+++ CRM/Nihrbackbone/Form/Report/SummaryNbr.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Nihrbackbone_Form_Report_SummaryNbr',
+    'entity' => 'ReportTemplate',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'SummaryNbr',
+      'description' => 'SummaryNbr (nihrbackbone)',
+      'class_name' => 'CRM_Nihrbackbone_Form_Report_SummaryNbr',
+      'report_url' => 'nihrbackbone/summarynbr',
+      'component' => '',
+    ),
+  ),
+);
