1ae3614dce680a666d8b72f3e0984542868a28d3
jab1012 <john.boucher@bioresource.nihr.ac.uk>
2020-11-04 15:16:58 +0000

    minor updates for 5118

--- api/v3/Distance/Calculate.php
+++ api/v3/Distance/Calculate.php
@@ -14,14 +14,12 @@ function _civicrm_api3_distance_Calculate_spec(&$spec) {
 }
 */
 function _civicrm_api3_distance_Calculate(&$spec) {
-
   $spec['postcode'] = [
     'api.required' =>1,
     'name' => 'postcode',
     'title' => 'postcode',
     'type' => CRM_Utils_Type::T_STRING,
   ];
-
 }
 
 /**
@@ -34,33 +32,53 @@ function _civicrm_api3_distance_Calculate(&$spec) {
  * @throws API_Exception
  */
 function civicrm_api3_distance_Calculate($params) {
-
   $from = str_replace(' ', '', $params['postcode_from']);
   $to = str_replace(' ', '', $params['postcode_to']);
-
   $url = 'http://www.mapquestapi.com/directions/v2/route?key=ge1sXrGxbNAcYEreGTxWFV8PAT0m7UWA&from='.$from.'&to='.$to.'&outFormat=json&unit=Miles&routeType=shortest&locale=en_GB';
   $data = file_get_contents($url);
   $data = json_decode($data);
   $distance = round($data->route->distance);
-  # it would seem that some postcodes are unknown to mapquest - if so drop last char of $from and try again ..
+  # it would seem that some postcodes are unknown to mapquest - if so try with general area of 'from' postcode ..
   if ($distance == 0) {
-    $from1 = substr($from, 0, -1);
-    $url = 'http://www.mapquestapi.com/directions/v2/route?key=ge1sXrGxbNAcYEreGTxWFV8PAT0m7UWA&from='.$from1.'&to='.$to.'&outFormat=json&unit=Miles&routeType=shortest&locale=en_GB';
+    $from_area = getPostCodeArea($from);
+    $url = 'http://www.mapquestapi.com/directions/v2/route?key=ge1sXrGxbNAcYEreGTxWFV8PAT0m7UWA&from='.$from_area.'&to='.$to.'&outFormat=json&unit=Miles&routeType=shortest&locale=en_GB';
     $data = file_get_contents($url);
     $data = json_decode($data);
     $distance = round($data->route->distance);
   }
-  # if that does not work - drop last char of $to and try again ..
+  # if that does not work - try general area of 'to' postcode ..
   if ($distance == 0) {
-    $to1 = substr($to, 0, -1);
-    $url = 'http://www.mapquestapi.com/directions/v2/route?key=ge1sXrGxbNAcYEreGTxWFV8PAT0m7UWA&from='.$from.'&to='.$to1.'&outFormat=json&unit=Miles&routeType=shortest&locale=en_GB';
+    #$to_area = substr($to, 0, -1);
+    $to_area = getPostCodeArea($to);
+    $url = 'http://www.mapquestapi.com/directions/v2/route?key=ge1sXrGxbNAcYEreGTxWFV8PAT0m7UWA&from='.$from.'&to='.$to_area.'&outFormat=json&unit=Miles&routeType=shortest&locale=en_GB';
     $data = file_get_contents($url);
     $data = json_decode($data);
     $distance = round($data->route->distance);
   }
-
+  # finally - try general area of 'from'  and 'to' postcodes ..
+  if ($distance == 0) {
+    $url = 'http://www.mapquestapi.com/directions/v2/route?key=ge1sXrGxbNAcYEreGTxWFV8PAT0m7UWA&from='.$from_area.'&to='.$to_area.'&outFormat=json&unit=Miles&routeType=shortest&locale=en_GB';
+    $data = file_get_contents($url);
+    $data = json_decode($data);
+    $distance = round($data->route->distance);
+  }
   return $distance;
-
 }
+
+/* return general area of a postcode */
+function getPostCodeArea($pcode){
+  $pcode = str_replace(' ', '', $pcode);
+  if(strlen($pcode) > 4){
+    if(is_numeric($pcode{strlen($pcode)-1})){
+      $pcode = substr($pcode, 0, 4);
+    }else{
+      $pcode = substr($pcode, 0, strlen($pcode)-3);
+    }
+    return $pcode;
+  }else{
+    return $pcode;
+  }
+}
+
 
 
